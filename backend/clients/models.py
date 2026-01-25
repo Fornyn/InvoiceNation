709fcc949b93a858e9a90e0effee3b9b53b062dc
unknown <Fornyn@proton.me>
2026-01-25 22:33:09 +0530

    Add optional phone field to Client model

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Client(models.Model):
     name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.name
